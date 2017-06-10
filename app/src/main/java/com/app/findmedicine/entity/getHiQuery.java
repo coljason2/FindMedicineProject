e46da08ddbe01d3de13eec7f0ace3de533b38978
JunChiChen <coljason2@gmail.com>
2017-06-10 14:32:08 +0800

    2017/06/10 fix getHiQuery

--- app/src/main/java/com/app/findmedicine/entity/getHiQuery.java
+++ app/src/main/java/com/app/findmedicine/entity/getHiQuery.java
@@ -20,52 +20,53 @@ public class getHiQuery {
     private Calendar cal = Calendar.getInstance();
     private final String urlcode = "http://www.nhi.gov.tw/Query/query1_list.aspx?Q1ID=";
     private final String urlname = "http://www.nhi.gov.tw/Query/query1_list.aspx?NameChinese=";
-    private final String time = "&StartYYY=" + (cal.get(Calendar.YEAR)-1911);
+    // private final String time = "&StartYYY=" + (cal.get(Calendar.YEAR) - 1911);
+    private final String type = "&Type=迄今";
     private List<data> objects = new ArrayList<data>();
-    int index = 0;
+    int index = -1;
+
     public List<data> getHiMedicine(String name) throws NullPointerException {
         try {
-            Document doc = Jsoup.connect(urlname + name + time).get();
-           // Log.d("getHiMedicine=",doc.toString());
-            for(Element n : doc.getElementsByClass("GridView")) {
+            Document doc = Jsoup.connect(urlname + name + type).get();
+            Log.d("getHiMedicine=", doc.toString());
+            for (Element n : doc.getElementById("gvQuery1Data").getElementsByTag("tr")) {
                 data m = new data();
-                Log.d("Element = ", n.toString());
-                m.setName(n.getElementById("gvQuery1Data_lblNameChinese_" + index).text());
-                m.setHidPrice("健保價："+n.getElementById("gvQuery1Data_lblPrice_" + index).text());
-                m.setStartyear("健保碼："+n.getElementById("gvQuery1Data_lblstartEndDate_" + index).text());
-                m.setCode("期間"+n.getElementById("gvQuery1Data_hlID_" + index).text());
-                objects.add(m);
+                if (index >= 0) {
+                    m.setName(n.getElementById("gvQuery1Data_lblNameChinese_" + index).text());
+                    m.setHidPrice("健保價：" + n.getElementById("gvQuery1Data_lblPrice_" + index).text());
+                    m.setStartyear("期間：" + n.getElementById("gvQuery1Data_lblstartEndDate_" + index).text());
+                    m.setCode("健保碼：" + n.getElementById("gvQuery1Data_hlID_" + index).text());
+                    objects.add(m);
+                }
                 index++;
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
+            //getHiMedicine(name);
             e.printStackTrace();
+            Log.e("Exception=", e.toString());
         }
-
-
         return objects;
     }
 
     public List<data> getHiCode(String code) throws NullPointerException {
         try {
-            Document doc = Jsoup.connect(urlcode + code + time).get();
-           // Log.d("getHiCode=",doc.toString());
-            for(Element n : doc.getElementsByClass("GridView")) {
+            Document doc = Jsoup.connect(urlcode + code + type).get();
+            for (Element n : doc.getElementById("gvQuery1Data").getElementsByTag("tr")) {
                 data m = new data();
-                Log.d("Element = ", n.toString());
-                m.setName(n.getElementById("gvQuery1Data_lblNameChinese_" + index).text());
-                m.setHidPrice("健保價："+n.getElementById("gvQuery1Data_lblPrice_" + index).text());
-                m.setStartyear("健保碼："+n.getElementById("gvQuery1Data_lblstartEndDate_" + index).text());
-                m.setCode("期間"+n.getElementById("gvQuery1Data_hlID_" + index).text());
-                objects.add(m);
+                if (index >= 0) {
+                    m.setName(n.getElementById("gvQuery1Data_lblNameChinese_" + index).text());
+                    m.setHidPrice("健保價：" + n.getElementById("gvQuery1Data_lblPrice_" + index).text());
+                    m.setStartyear("期間：" + n.getElementById("gvQuery1Data_lblstartEndDate_" + index).text());
+                    m.setCode("健保碼：" + n.getElementById("gvQuery1Data_hlID_" + index).text());
+                    objects.add(m);
+                }
                 index++;
             }
 
 
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e("Exception=", e.toString());
         }
-
-
         return objects;
     }
 }
